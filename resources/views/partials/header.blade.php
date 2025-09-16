1af1c11852ecbc1baeed40162ee3778af3049693
fedebacci <federicobacci1999@gmail.com>
2025-09-16 12:46:34 +0200

    header layout and style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,45 @@
-<header>
+<section class="shadow-sm mb-4">
     <div class="container">
-        Header
+        <header class="d-flex flex-wrap justify-content-center">
+            <a 
+                href="{{ route("home") }}" 
+                class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+            >
+                <img
+                    src="{{ Vite::asset('resources/img/dc-logo.png') }}"
+                    alt="DC Logo"
+    
+                    id="site-logo"
+                    class="img-fluid my-3"
+                />
+            </a>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link h-100 d-flex align-items-center active" aria-current="page">
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link h-100 d-flex align-items-center">
+                        Features
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link h-100 d-flex align-items-center">
+                        Pricing
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link h-100 d-flex align-items-center">
+                        FAQs
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link h-100 d-flex align-items-center">
+                        About
+                    </a>
+                </li>
+            </ul>
+        </header>
     </div>
-</header>
+</section>
